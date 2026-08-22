732495cff268a785af9f461119a66daf22004eb9
Rafael Choi <yunochoi0402@gmail.com>
2026-08-22 01:36:54 -0300

    feat: adiciona theme context e preferência do usuário

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,9 +1,12 @@
-import { Clock, TrendingUp, Wallet } from "lucide-react";
+import { Clock, Moon, Sun, TrendingUp, Wallet } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { Button } from "./Button";
+import { useTheme } from "../../hooks/useTheme";
+import { Divider } from "./Divider";
 
 export function Header() {
     const navigate = useNavigate();
+    const { theme, toggleTheme } = useTheme()
 
     return (
         <header className="border-b border-(--border) px-6 py-3">
@@ -26,6 +29,13 @@ export function Header() {
                     <Button variant="ghost" icon={Clock} onClick={() => void navigate('/historico')}>
                         <span className="hidden sm:inline">Histórico</span>
                     </Button>
+                    <Divider orientation="vertical" />
+                    <Button
+                        aria-label={`Mudar para tema ${theme === 'light' ? 'escuro' : 'claro'}`}
+                        variant="ghost"
+                        icon={theme === 'light' ? Moon : Sun}
+                        onClick={toggleTheme}   
+                    />
                 </div>
             </nav>
             Header

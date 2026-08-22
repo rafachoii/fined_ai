c5cf89ac089f6e9ceef327330c8c0437cf3746ff
Rafael Choi <yunochoi0402@gmail.com>
2026-08-22 00:49:54 -0300

    feat: adiciona header e cria o layout base da aplicação

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
+import { RootLayout } from "./components/layout/RootLayout";
 
 export const router = createBrowserRouter([
     {
+        element: <RootLayout />,
         children: [
             {
                 path: '/',
